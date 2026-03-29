c7a8eca933ae1088298b1b1364341a5e1f5f8f36
iyedmilouchi <iyed.milouchi@esprit.tn>
2026-03-29 17:26:07 +0100

    Structure module TrendyPlaces - entities

--- backend/src/main/java/org/example/backend_tunisiahub/Entities/TrendyPlaces/ActiviteLieu.java
+++ backend/src/main/java/org/example/backend_tunisiahub/Entities/TrendyPlaces/ActiviteLieu.java
@@ -1,4 +1,30 @@
 package org.example.backend_tunisiahub.Entities.TrendyPlaces;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class ActiviteLieu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomActivite;
+    private String description;
+    private Double prix;
+    private Integer duree;
+    private Integer capaciteMax;
+    private Boolean disponible;
+
+    @ManyToOne
+    @JoinColumn(name = "lieu_id")
+    @JsonIgnoreProperties("activites")
+    private Lieu lieu;
 }
--- backend/src/main/java/org/example/backend_tunisiahub/Entities/TrendyPlaces/Lieu.java
+++ backend/src/main/java/org/example/backend_tunisiahub/Entities/TrendyPlaces/Lieu.java
@@ -1,4 +1,33 @@
 package org.example.backend_tunisiahub.Entities.TrendyPlaces;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Lieu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nom;
+    private String description;
+    private String type;
+    private String ville;
+    private String image;
+    private Double latitude;
+    private Double longitude;
+    private String horaires;
+
+    @OneToMany(mappedBy = "lieu", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("lieu")
+    private List<ActiviteLieu> activites;
 }
