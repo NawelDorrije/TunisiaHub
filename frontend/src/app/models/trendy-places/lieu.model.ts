2c8ccd777609e169be381c3b093598152daff59f
iyedmilouchi <iyed.milouchi@esprit.tn>
2026-03-29 18:14:23 +0100

    models & service

---
+++ frontend/src/app/models/trendy-places/lieu.model.ts
@@ -0,0 +1,22 @@
+export interface ActiviteLieu {
+  id: number;
+  nomActivite: string;
+  description: string;
+  prix: number;
+  duree: number;
+  capaciteMax: number;
+  disponible: boolean;
+}
+
+export interface Lieu {
+  id: number;
+  nom: string;
+  description: string;
+  type: string;
+  ville: string;
+  image: string;
+  latitude: number;
+  longitude: number;
+  horaires: string;
+  activites?: ActiviteLieu[];
+}
--- frontend/src/app/services/trendy-places.service.ts
+++ frontend/src/app/services/trendy-places.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Lieu, ActiviteLieu } from '../models/trendy-places/lieu.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrendyPlacesService {
+  private BASE_URL = 'http://localhost:8089';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getAllLieux(): Observable<Lieu[]> {
+    return this.http.get<Lieu[]>(`${this.BASE_URL}/api/lieux`);
+  }
+
+  getLieuById(id: number): Observable<Lieu> {
+    return this.http.get<Lieu>(`${this.BASE_URL}/api/lieux/${id}`);
+  }
+
+  getActivitesByLieu(lieuId: number): Observable<ActiviteLieu[]> {
+    return this.http.get<ActiviteLieu[]>(`${this.BASE_URL}/api/activites/lieu/${lieuId}`);
+  }
 }
